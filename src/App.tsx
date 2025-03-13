76ba3ffde6ba7ff33f59ff170488d08703feabaa
Le Thanh Lam <lamlt@Mac-mini-cua-Le.local>
2025-03-13 23:11:28 +0700

    initate project

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Gà Chọi Livestream - Đang phát triển
+        </p>
+        <a
+          className="App-link"
+          href="https://www.fb.com/lam.lethanh.fis"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Liên hệ tác giả
+        </a>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
