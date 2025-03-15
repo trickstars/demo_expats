69f323eb9ab421b02a429cdae2054b307ffdb0ec
trickstars <nguyenminhkhoe123@gmail.com>
2025-03-15 21:46:39 +0700

    header-desktop

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'
+import VideoList from './pages/VideoList';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -9,15 +10,16 @@ import Footer from './components/Footer';
 function App() {
   return (
     <>
-    <Header></Header>
     <Router>
+    <Header></Header>
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path='/video-xo-ga' element={<VideoList />} />
         </Routes>
       </div>
-    </Router>
     <Footer></Footer>
+    </Router>
     </>
   );
 }
