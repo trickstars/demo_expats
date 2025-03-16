8897f146d2dc072d7f687fbb8672c54cf14cad97
trickstars <nguyenminhkhoe123@gmail.com>
2025-03-16 16:47:51 +0700

    add description area (not done)

---
+++ src/components/Description/index.tsx
@@ -0,0 +1,39 @@
+function Description() {
+    return (
+        <div id="description-area" className="description-area">
+            <div id="description-col" className="flex-col">
+                <div id="description-content">
+                    <div className="icon-box">
+                        <div className="icon-box-img"></div>
+                        <div className="icon-box-content">
+                            <h4>CLB Gà Chọi Lê Tuấn</h4>
+                            <p>Xổ vần gà trực tiếp 18h30 hàng ngày tại Khu Phố Phú Thứ, Thị Trấn Phú Thứ, Huyện Tây Hòa, Tỉnh Phú Yên
+                            Hotline liên hệ: 0345357695</p>
+                        </div>
+                    </div>
+
+                    <div className="description">
+                        <p>
+                            Chào mừng bạn đến với gachoitieuphong.com
+                            nơi tạo ra sân chơi phục vụ niềm đam mê gà đòn cho anh em 24/7. 
+                            Đây là website của CLB Gà Chọi Tiêu Phong
+                        </p>
+                        <p>
+                        Tại CLB Gà Chọi Tiêu Phong bạn có thể tìm hiểu về kiến thức về gà đòn,
+                         hay đơn giản là thưởng thức những video xổ gà trong những lúc rảnh rỗi. 
+                         CLB Gà Chọi Tiêu Phong sẽ đưa đến cho bạn những thông tin mới nhất về giống gà đòn, 
+                         kinh nghiệm chăm sóc gà, cách huấn luyện gà chọi và nhiều hơn thế nữa. 
+                         Ngoài ra, gachoitieuphong.com còn cung cấp con giống gà đòn cho những ai đang quan tâm đến việc nuôi gà đòn.
+                        </p>
+                    </div>
+                </div>
+                <div id="map-link"></div>
+                <div id="tiktok"></div>
+            </div>
+            <div id="slide" className="flex-col"></div>
+            <div id="contact-info" className="flex-col"></div>
+        </div>
+    )
+} 
+
+export default Description
